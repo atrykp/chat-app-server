6487c1c16decd97db791500510e2e191fa66aa06
Patryk Plaza <atrykp0@gmail.com>
2021-07-16 06:51:41 +0200

    create add message route

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -1,5 +1,21 @@
-const createMessage = async (req, res) => {
-  res.send("hello");
-};
+const asyncHandler = require("express-async-handler");
+const Message = require("../models/message-model");
+
+const createMessage = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+  const conversationId = req.body.conversationId;
+  const message = await Message.create({
+    conversationId,
+    sender: userId,
+    text: req.body.text,
+  });
+
+  if (!message) {
+    res.status(400);
+    throw new Error("couldnt send message");
+  }
+
+  res.send(message);
+});
 
 module.exports.createMessage = createMessage;
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const protect = require("../middlewares/protect");
 const message = require("../controllers/messageControllers");
 const messageRoute = express.Router();
 
-messageRoute.route("/").post(message.createMessage);
+messageRoute.route("/").post(protect, message.createMessage);
 
 module.exports = messageRoute;
